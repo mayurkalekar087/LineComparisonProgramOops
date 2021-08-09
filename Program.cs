8d556fe9300b078ca5ff3aaa058ac5ed861013ef
Mayur Mahadev Kalekar <87481060+mayurkalekar087@users.noreply.github.com>
2021-08-09 13:58:14 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,11 +8,11 @@ namespace LineComparison
         {
             Console.WriteLine("Welcome To Line Comparison Computation Program!");
             Distance obj = new Distance(1, 2, 1, 2);
-            double LineOne = obj.Compair();
+            double LineOne = obj.Compare();
             Console.WriteLine(LineOne);
 
             Distance obj1 = new Distance(1, 2, 1, 2);
-            double LineTwo = obj1.Compair();
+            double LineTwo = obj1.Compare();
             Console.WriteLine(LineTwo);
 
             Distance.Equal(LineOne, LineTwo);
